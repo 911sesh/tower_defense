d4334c43743977687c9000cca8ff4df6283ebbc9
ivan <Ivan.artemyev1410@yandex.ru>
2023-09-03 12:31:32 +0300

    class Block cells now include blocks added new subclasses(Stone, Sand) added durability to subclasses

--- cell.py
+++ cell.py
@@ -7,6 +7,31 @@ class Block(pg.sprite.Sprite):
         self.rect = self.image.get_rect(topleft=pos)
         self.image.fill('red')
 
+    def update(self, screen):
+        screen.blit(self.image, self.rect)
+
+class Stone(Block):
+    def __init__(self, pos):
+        super().__init__(pos)
+        self.image = pg.Surface((32, 32))
+        self.rect = self.image.get_rect(topleft=pos)
+        self.image.fill((180, 180, 225))
+        self.durability = 6
 
     def update(self, screen):
         screen.blit(self.image, self.rect)
+
+class Sand(Block):
+    def __init__(self, pos):
+        super().__init__(pos)
+        self.image = pg.Surface((32, 32))
+        self.rect = self.image.get_rect(topleft=pos)
+        self.image.fill((255, 204, 92))
+        self.durability = 3
+
+    def update(self, screen):
+        screen.blit(self.image, self.rect)
+
+
+
+
