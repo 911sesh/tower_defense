237a2b5843502e0d7b92d5e11ad8051740d3530f
ivan <Ivan.artemyev1410@yandex.ru>
2023-10-12 20:56:42 +0300

    changed filler value

--- building_map.py
+++ building_map.py
@@ -7,12 +7,11 @@ class Cell(pg.sprite.Sprite):
     def __init__(self, size: int, x: int, y: int, *groups):
         super().__init__(*groups)
         self.rect = pg.Rect(x, y, size, size)
-        self.filler = 1
         self.color = (20, 20, 20)
         self.available = False
 
     def draw(self, surface):
-        pg.draw.rect(surface, self.color, self.rect, self.filler)
+        pg.draw.rect(surface, self.color, self.rect, 1)
 
     def available_animation(self, player):
         if self.rect.colliderect(player.building_zone) and not self.rect.colliderect(player.rect):
